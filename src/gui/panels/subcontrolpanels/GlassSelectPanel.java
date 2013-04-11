d4a09861d96965acf7b901ef7dbf17aa1c95c1d0
David Zhang <dzhang21@gmail.com>
2013-04-11 16:12:56 -0700

    started glass select panel

--- src/gui/panels/subcontrolpanels/GlassSelectPanel.java
+++ src/gui/panels/subcontrolpanels/GlassSelectPanel.java
@@ -2,7 +2,13 @@ package gui.panels.subcontrolpanels;
 
 import gui.panels.ControlPanel;
 
+import java.util.ArrayList;
+
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import shared.enums.MachineType;
 
 /**
  * The GlassSelectPanel class contains buttons allowing the user to select what type of glass to produce.
@@ -12,6 +18,10 @@ public class GlassSelectPanel extends JPanel {
 	/** The ControlPanel this is linked to */
 	private ControlPanel parent;
 
+	private JScrollPane scrollPane;
+	private JPanel mainPanel;
+	private ArrayList<JCheckBox> checkBoxes;
+	
 	/**
 	 * Creates a new GlassSelect and links it to the control panel
 	 * 
@@ -19,6 +29,29 @@ public class GlassSelectPanel extends JPanel {
 	 */
 	public GlassSelectPanel(ControlPanel cp) {
 		parent = cp;
+
+		checkBoxes = new ArrayList<JCheckBox>();
+		checkBoxes.add(new JCheckBox(MachineType.CROSS_SEAMER.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.DRILL.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.GRINDER.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.MANUAL_BREAKOUT.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.CUTTER.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.WASHER.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.UV_LAMP.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.OVEN.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.PAINT.toString()));
+		checkBoxes.add(new JCheckBox(MachineType.BREAKOUT.toString()));
+
+
+		mainPanel = new JPanel();
+		//TODO: SETLAYOUT
+		scrollPane = new JScrollPane(this);
+		scrollPane.add(mainPanel);
+
+		for (int i=0; i<checkBoxes.size(); i++) {
+			
+		}
+
 	}
 
 	/**
