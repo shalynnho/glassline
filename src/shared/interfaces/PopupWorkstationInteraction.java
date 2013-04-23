fd4f1be2c85435e2c1c47d78bfa5d082ae77e1c3
Evan Brown <ebrown255@gmail.com>
2013-04-22 17:52:53 -0700

    Changed PopupWorkstationInteraction interface.

--- src/shared/interfaces/PopupWorkstationInteraction.java
+++ src/shared/interfaces/PopupWorkstationInteraction.java
@@ -5,5 +5,6 @@ import shared.Glass;
 /* Shared interface for the Workstation to interact with various popup agent implementations. */
 public interface PopupWorkstationInteraction extends NonnormBreakInteraction {
 	public void msgGlassDone(Glass g, int index);
-	public void msgGUIBreakWorkstation(boolean stop, int machineIndex);
+	public void msgGUIBreakWorkstation(boolean stop, int index);
+	public void msgGUIBreakRemoveGlassFromWorkstation(int index);
 }
