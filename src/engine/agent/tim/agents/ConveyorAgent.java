886b2c023ed2a2723f9bf9b968f7c72dfd4d3b65
timrighettini <tim.righettini@gmail.com>
2013-04-23 12:12:26 -0700

    Fixed a bug in my conveyor breaking where the glass would be stuck on the conveyor when the glass was past the entry sensor but before the popUp sensor.

--- src/engine/agent/tim/agents/ConveyorAgent.java
+++ src/engine/agent/tim/agents/ConveyorAgent.java
@@ -272,7 +272,23 @@ public class ConveyorAgent extends Agent implements Conveyor {
 	//Other Methods:
 	@Override
 	public void eventFired(TChannel channel, TEvent event, Object[] args) {
-		// N/A
+		if (channel == TChannel.POPUP/* && event == TEvent.POPUP_GUI_RELEASE_FINISHED*/) {
+			// Non-Norm check -- if there is no glass on the PopUpSensor and there is no glass on the popUp, turn on conveyor if it is stuck after popUp releases a piece of glass
+			boolean glassOnPopUpSensor = false;
+			synchronized(glassSheets) {
+				for (MyGlassConveyor g: glassSheets) {
+					if (g.conveyorState == conveyorState.onPopUpSensor || g.conveyorState == conveyorState.beforePopUp) {
+						glassOnPopUpSensor = true;
+						break;
+					}
+				}
+			}		
+			
+			
+			if (!glassOnPopUpSensor) { // Turn on the conveyor, so the glass can make it to the popUp sensor
+				turnOnConveyorGUI();
+			}
+		}
 	}
 	
 	// Methods that turn the GUI conveyor on or off	
